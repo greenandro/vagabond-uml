cbf88028d04c8908409d9e63a4bbb08ac5ca7ec7
Hakan Dilek <hakandilek@gmail.com>
2016-03-30 16:56:35 +0200

    Complete annotation model for entities

--- test/dsl/dsl_parser_test.js
+++ test/dsl/dsl_parser_test.js
@@ -108,6 +108,94 @@ describe("DSL Parser", function(){
 
     });
 
+    describe("When entities have annotations", function(){
+      
+      it("the annotations in result are well formed",function(){
+       //Book
+       expect(parserAnnotations.result.entities[0].annotations.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[0].annotations[0].name).to.be.equal("paginate");
+       expect(parserAnnotations.result.entities[0].annotations[0].parameters.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[0].annotations[0].parameters[0].name).to.be.equal("pagination");
+       expect(parserAnnotations.result.entities[0].annotations[0].parameters[0].value).to.be.equal(true);
+       //MultipleAnnotations
+       expect(parserAnnotations.result.entities[1].annotations.length).to.be.equal(2);
+       expect(parserAnnotations.result.entities[1].annotations[0].name).to.be.equal("dto");
+       expect(parserAnnotations.result.entities[1].annotations[0].parameters.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[1].annotations[0].parameters[0].name).to.be.equal("mapstruct");
+       expect(parserAnnotations.result.entities[1].annotations[0].parameters[0].value).to.be.equal(true);
+       expect(parserAnnotations.result.entities[1].annotations[1].name).to.be.equal("paginate");
+       expect(parserAnnotations.result.entities[1].annotations[1].parameters.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[1].annotations[1].parameters[0].name).to.be.equal("infinite-scroll");
+       expect(parserAnnotations.result.entities[1].annotations[1].parameters[0].value).to.be.equal(true);
+       //A1
+       expect(parserAnnotations.result.entities[2].annotations.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[2].annotations[0].name).to.be.equal("noParams");
+       expect(parserAnnotations.result.entities[2].annotations[0].parameters.length).to.be.equal(0);
+       //A2
+       expect(parserAnnotations.result.entities[3].annotations.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[3].annotations[0].name).to.be.equal("noParams");
+       expect(parserAnnotations.result.entities[3].annotations[0].parameters.length).to.be.equal(0);
+       //B1
+       expect(parserAnnotations.result.entities[4].annotations.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[4].annotations[0].name).to.be.equal("multiParams");
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters.length).to.be.equal(3);
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters[0].name).to.be.equal("a");
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters[0].value).to.be.equal(true);
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters[1].name).to.be.equal("B");
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters[1].value).to.be.equal(true);
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters[2].name).to.be.equal("c1");
+       expect(parserAnnotations.result.entities[4].annotations[0].parameters[2].value).to.be.equal(true);
+       //B2
+       expect(parserAnnotations.result.entities[5].annotations.length).to.be.equal(1);
+       expect(parserAnnotations.result.entities[5].annotations[0].name).to.be.equal("multiParams");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters.length).to.be.equal(8);
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[0].name).to.be.equal("a");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[0].value).to.be.equal(true);
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[1].name).to.be.equal("B");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[1].value).to.be.equal("asd");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[2].name).to.be.equal("c");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[2].value).to.be.equal(42);
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[3].name).to.be.equal("d");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[3].value).to.be.equal(3.41);
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[4].name).to.be.equal("e");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[4].value).to.be.equal(true);
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[5].name).to.be.equal("f");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[5].value).to.be.equal("EN");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[6].name).to.be.equal("g");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[6].value).to.be.equal(null);
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[7].name).to.be.equal("h");
+       expect(parserAnnotations.result.entities[5].annotations[0].parameters[7].value).to.be.equal(true);
+      });
+
+      it("the annotations are well formed",function(){
+       parserAnnotations.fillEnums();
+       parserAnnotations.fillClassesAndFields();
+       
+       expect(parserAnnotations.parsedData.getClass('Book').getAnnotation("paginate").name).to.be.equal("paginate");
+       expect(parserAnnotations.parsedData.getClass('Book').getAnnotation("paginate").getParameter("pagination")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('MultipleAnnotations').getAnnotation("dto").name).to.be.equal("dto");
+       expect(parserAnnotations.parsedData.getClass('MultipleAnnotations').getAnnotation("dto").getParameter("mapstruct")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('MultipleAnnotations').getAnnotation("paginate").name).to.be.equal("paginate");
+       expect(parserAnnotations.parsedData.getClass('MultipleAnnotations').getAnnotation("paginate").getParameter("infinite-scroll")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('A1').getAnnotation("noParams").name).to.be.equal("noParams");
+       expect(parserAnnotations.parsedData.getClass('A2').getAnnotation("noParams").name).to.be.equal("noParams");
+       expect(parserAnnotations.parsedData.getClass('B1').getAnnotation("multiParams").name).to.be.equal("multiParams");
+       expect(parserAnnotations.parsedData.getClass('B1').getAnnotation("multiParams").getParameter("a")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('B1').getAnnotation("multiParams").getParameter("B")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('B1').getAnnotation("multiParams").getParameter("c1")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").name).to.be.equal("multiParams");
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("a")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("B")).to.be.equal("asd");
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("c")).to.be.equal(42);
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("d")).to.be.equal(3.41);
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("e")).to.be.equal(true);
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("f")).to.be.equal("EN");
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("g")).to.be.equal(null);
+       expect(parserAnnotations.parsedData.getClass('B2').getAnnotation("multiParams").getParameter("h")).to.be.equal(true);
+      });
+
+    });
+
   });
 
   describe("#fillAssociations", function(){
